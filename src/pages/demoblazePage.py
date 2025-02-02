cd4811660318ca368f980bae454487c73b678a28
Amechiw3 <martin.fierro.dev@gmail.com>
2025-02-02 13:39:44 -0700

    Add initial test structure and configuration for Demoblaze page tests

--- src/pages/demoblazePage.py
+++ src/pages/demoblazePage.py
@@ -1,10 +1,10 @@
 from selenium.webdriver.common.by import By
+from utils.logger import setup_logger
 from utils.wait_utils import WaitUtils
 from pages.base_page import BasePage
 
-class GooglePage(BasePage):
-    TEXTAREA_SEARCH = (By.XPATH, "//textarea[@class='gLFyf']")
-    LOGIN_BUTTON = (By.XPATH, "//input[@class='gNO89b']")
+class demoblazePage(BasePage):
+    
 
     def __init__(self, driver, config, logger_name, screenshotDIR, log_DIR, testName = ""):
         self.driver = driver
@@ -13,10 +13,12 @@ class GooglePage(BasePage):
         self.screenshotDIR = screenshotDIR
         self.log_DIR = log_DIR
         self.testName = testName
+
+        self.logger = setup_logger(logger_name, log_DIR)
+        self.wait = WaitUtils(driver, config)
+        
     
-    def goto(self):
+    def go_to_page(self):
         self.navigate(self.config['base_url'])
+        self.logger.info(f"Opening the page: {self.config['base_url']}")
     
-    def search(self, busqueda):
-        self.send_keys(self.TEXTAREA_SEARCH, busqueda)
-        self.click(self.LOGIN_BUTTON)
---
+++ src/tests/demoblaze/test_demoblaze.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+import time
+import pytest
+from utils.wait_utils import WaitUtils
+from pages.demoblazePage import demoblazePage
+
+@pytest.mark.google
+class TestDemoblaze:
+    log_name = "TestDemoblaze_Logger"
+    log_date = datetime.now().date()
+    log_datetime = datetime.now().strftime("%Y-%m-%d %H.%M.%S")
+    screenshotDIR = f"Demoblaze/Demoblaze_{log_date}/Demoblaze_{log_datetime}"
+    log_DIR = f"Demoblaze/Demoblaze_{log_date}/Demoblaze_{log_datetime}"
+
+
+    def test01_Go_To_Page(self, driver, config):
+        testName = "Test01_Go_To_PAGE"
+        self.log_name = f"{self.log_name}_{testName}"
+        self.screenshotDIR = f"{self.screenshotDIR}/{testName}"
+        self.log_DIR = f"{self.log_DIR}/{testName}"
+
+        demoblaze = demoblazePage(driver, config, self.log_name, self.screenshotDIR, self.log_DIR, testName)
+        demoblaze.go_to_page()
+        time.sleep(10)
+        
+        assert True
